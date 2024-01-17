9d485fc4e78cb766b519fbd7899be86439748a43
potato0221 <wnqo10@naver.com>
2024-01-17 10:30:57 +0900

    setting

---
+++ src/main/java/com/ll/app20240117/App20240117Application.java
@@ -0,0 +1,13 @@
+package com.ll.app20240117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20240117Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20240117Application.class, args);
+    }
+
+}
